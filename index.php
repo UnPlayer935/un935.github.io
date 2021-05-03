14d2ce1b291500166198cf065abf9ee6b76dbff6
UnPlayer935 <83595000+UnPlayer935@users.noreply.github.com>
2021-05-04 00:30:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html>
+  <head>
+    <style>
+      div{
+  width: 200px;
+  height: 200px;
+  border: 3px solid black;
+}
+      </style>
+    </head>
 <body>
-
-<?php
-$txt1 = "Learn PHP";
-$txt2 = "W3Schools.com";
-$x = 5;
-$y = 4;
-
-echo "<h2>" . $txt1 . "</h2>";
-echo "Study PHP at " . $txt2 . "<br>";
-echo $x + $y;
+  <div id="drag1">drag me</div>
+  <?php
+  $('#drag1').draggable();
 ?>
 
 </body>
